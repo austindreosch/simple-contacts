24369c24d1169e00bc96f82e11eb497c739c6081
Austin Dreosch <austindreosch@gmail.com>
2024-05-15 19:22:25 -0700

    Added nav styling

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,94 @@ getUser();
 </script>
 
 <template>
-  <div>
+
     <!-- NAV -->
-    <nav class="p-4 flex justify-center fixed pt-10 pb-7 top-0 left-0 right-0 bg-orange-200 z-10">
-      <!-- Logo (Home) -->
+  <header class="bg-orange-200">
+    <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+
+        <!-- LEFT SIDE OF NAV -->
+        <div class="flex-1 md:flex md:items-center md:gap-12">
+          <router-link class="block text-gray-800" to="/">
+            <span class="sr-only">Home</span>
+            <div class="font-outfit text-black-800 flex space-x-2 items-center pr-20">
+              <span class="mt-[.45rem] pl-0.5">
+                <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d="M4 4a2 2 0 1 0 0 4h16a2 2 0 1 0 0-4H4Zm0 6h16v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-8Zm10.707 5.707a1 1 0 0 0-1.414-1.414l-.293.293V12a1 1 0 1 0-2 0v2.586l-.293-.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l2-2Z" clip-rule="evenodd"/>
+                </svg>
+              </span>
+              <h1 class="text-4xl font-semibold">simple</h1>
+              <h1 class="text-4xl font-semibold ">contacts</h1>
+            </div>
+          </router-link>
+        </div>
+        
+        <!-- RIGHT SIDE OF NAV -->
+        <div class="md:flex md:items-center md:gap-12">
+          <!-- PAGES -->
+          <nav aria-label="Global" class="hidden md:block">
+            <ul class="flex items-center gap-6 text-sm">
+              <li>
+                <router-link class="text-gray-500 transition hover:text-gray-500/75"to="/"> Home </router-link>
+              </li>
+              <li>
+                <router-link class="text-gray-500 transition hover:text-gray-500/75"to="/add"> Add an Individual Contact </router-link>
+              </li>
+              <li>
+                <router-link class="text-gray-500 transition hover:text-gray-500/75" to="/import"> Import Contacts </router-link>
+              </li>
+            </ul>
+          </nav>
+
+          <div class="flex items-center gap-4">
+            <!-- LOGIN+REGISTER/PROFILE -->
+            <div class="sm:flex sm:gap-4">
+              <router-link
+                class="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                to="/auth"
+              >
+                Login
+              </router-link>
+  
+              <div class="hidden sm:flex">
+                <router-link
+                  class="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+                  to="/auth"
+                >
+                  Register
+                </router-link>
+              </div>
+            </div>
+
+            <!-- MOBILE SELECTOR -->
+            <div class="block md:hidden">
+              <button class="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+    </div>
+  </header>
+
+
+
+
+    <!-- <nav class="p-4 flex justify-center fixed pt-10 pb-7 top-0 left-0 right-0 bg-orange-200 z-10">
+
       <router-link class="text-black" to="/">
         <div class="font-outfit text-black-800 flex space-x-2 items-center pr-20">
           <h1 class="text-4xl font-semibold">simple</h1>
@@ -20,18 +104,18 @@ getUser();
           <h1 class="text-4xl font-semibold ">contacts</h1>
         </div>
       </router-link>
-      <!-- Other Links -->
       <div class="flex justify-between space-x-4 pt-1">
         <router-link class="px-4 py-2 bg-blue-400 text-white rounded hover:bg-white hover:text-black" to="/add">Add Invidual Contact</router-link>
         <router-link class="px-4 py-2 bg-blue-400 text-white rounded hover:bg-white hover:text-black" to="/import">Import Contacts</router-link>
         <router-link class="px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-400 hover:text-black" to="/auth">{{ user ? user.email : 'Login' }}</router-link>
       </div>
-    </nav>
+    </nav> -->
+
+
     <!-- BODY -->
-    <main class="max-w-10xl mt-32"> 
+    <main class="max-w-10xl mt-32 px-4 mx-auto"> 
       <router-view></router-view>
     </main>
-  </div>
 </template>
 
 
