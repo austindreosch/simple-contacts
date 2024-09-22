bb78dbe6627e0032164a3e245b3e6fb936240d71
Austin Dreosch <austindreosch@gmail.com>
2024-09-21 20:39:37 -0700

    2

--- src/components/ListsBlock.vue
+++ src/components/ListsBlock.vue
@@ -117,54 +117,58 @@ const formatDate = (dateString) => {
 
 </script>
 
+
+
+
+
+
 <template>
-    <!-- Details -->
-    <div class="flex justify-between items-end my-2">
-        <h1 class="text-left text-2xl mt-2">Groups</h1>
-        <div>
-            <span class="inline-flex rounded-md border bg-white shadow-sm">
-                <button
-                    class="flex items-center gap-1 px-3 py-1.5 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:relative border-r border-gray-200"
-                    :class="showNewListForm ? 'bg-my-teal text-white hover:bg-teal-600 border border-teal-600 rounded-l-md' : ''"
-                    title="View Orders"
-                    @click="showNewListForm = !showNewListForm"
-                    >
-                    New
-                    <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" :fill="showNewListForm ? 'white' : '#666666'" ><path d="M144-396v-72h288v72H144Zm0-150v-72h432v72H144Zm0-150v-72h432v72H144Zm492 456v-156H480v-72h156v-156h72v156h156v72H708v156h-72Z"/></svg> 
-                </button>
-                <SortDropdown />
-            </span>
-        </div>
-
-
-    </div>
-
-    
-    
-    
-    <!-- Lists -->
-    <div class="bg-white border border-gray-300 rounded-lg shadow-md w-full px-4 pt-4 space-y-2 select-none ">
-
-        <!-- Add New List Form -->
-        <div v-if="showNewListForm" class="mb-2 flex items-center">
-            <input
-                type="text"
-                v-model="newListName"
-                placeholder="Enter new group name..."
-                class="flex-grow px-3 py-2 border rounded-md mr-1"
-            />
-
+<!-- Title/Buttons -->
+<div class="flex justify-between items-end my-2">
+    <h1 class="text-left text-2xl mt-2">Groups</h1>
+    <div>
+        <span class="inline-flex rounded-md border bg-white shadow-sm">
             <button
-                @click="addNewList"
-                class="bg-my-teal text-white px-3 py-2 rounded-md border border-green-700 hover:bg-my-teal-dark"
-            >
-                Add Group
+                class="flex items-center gap-1 px-3 py-1.5 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:relative border-r border-gray-200"
+                :class="showNewListForm ? 'bg-my-teal text-white hover:bg-teal-600 border border-teal-600 rounded-l-md' : ''"
+                title="View Orders"
+                @click="showNewListForm = !showNewListForm"
+                >
+                New
+                <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" :fill="showNewListForm ? 'white' : '#666666'" ><path d="M144-396v-72h288v72H144Zm0-150v-72h432v72H144Zm0-150v-72h432v72H144Zm492 456v-156H480v-72h156v-156h72v156h156v72H708v156h-72Z"/></svg> 
             </button>
-        </div>
-        <div v-if="showNewListForm">
+            <SortDropdown />
+        </span>
+    </div>
+</div>
+
+
+<div class="bg-white border border-gray-300 rounded-lg shadow-md w-full px-4 pt-4 space-y-2 select-none ">
+    <!-- ///////////////// -->
+    <!-- Add New List Form if showNewListForm (this should be a search bar otherwise?) -->
+    <div v-if="showNewListForm" class="mb-2 flex items-center">
+        <input
+        type="text"
+        v-model="newListName"
+        placeholder="Enter new group name..."
+        class="flex-grow px-3 py-2 border rounded-md mr-1"
+        />
+        <button
+        @click="addNewList"
+        class="bg-my-teal text-white px-3 py-2 rounded-md border border-green-700 hover:bg-my-teal-dark"
+        >
+        Add Group
+        </button>
+
+        <div class="flex mb-1">
             <hr class="my-2 border-gray-300" />
         </div>
+    </div>
 
+    <!-- ///////////////// -->
+    <!-- Block - Lists View -->
+    <div v-if="!showListDetails">
+        <!-- List Buttons -->
         <div v-for="list in paginatedLists" :key="list.id">            
             <div class="relative block rounded-md border-2  border-teal-600 shadow-lg p-2 bg-my-teal ">
                 <div class="grid grid-cols-20 justify-between items-center h-full px-1 pl-2">
@@ -181,67 +185,82 @@ const formatDate = (dateString) => {
                 </div>
             </div>
         </div>
-            
-
-
-        <!-- PAGINATION -->
-        <ol class="flex justify-center gap-1 text-xs font-medium pt-2 pb-4">
-            <li>
-                <button
-                    @click="prevPage"
-                    :disabled="currentListPage === 1"
-                    class="inline-flex size-8 items-center justify-center rounded border border-gray-2 00 bg-gray-100 text-gray-900 rtl:rotate-180"
-                >
-                    <span class="sr-only">Prev Page</span>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-3 w-3"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </button>
-            </li>
-
-            <li v-for="page in totalPages" :key="page">
-                <button
-                    @click="goToPage(page)"
-                    :class="{'block size-8 rounded border border-gray-2 00 bg-gray-100 text-center leading-8 text-gray-900': page !== currentListPage, 'block size-8 rounded border-my-teal bg-my-teal text-center leading-8 text-white': page === currentListPage}"
-                >
-                    {{ page }}
-                </button>
-            </li>
-
-            <li>
-                <button
-                    @click="nextPage"
-                    :disabled="currentListPage === totalPages"
-                    class="inline-flex size-8 items-center justify-center rounded border border-gray-200 bg-gray-100 text-gray-900 rtl:rotate-180"
-                >
-                    <span class="sr-only">Next Page</span>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-3 w-3"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a 1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </button>
-            </li>
-        </ol>
-
-        
-
-
     </div>
+
+    <!-- ///////////////// -->
+    <!-- Block - Detail View -->
+    <div v-if="showListDetails" class="bg-white border border-gray-300 rounded-lg shadow-md w-full px-4 pt-4 space-y-2 select-none">
+        <div class="flex justify-between items-center mb-4">
+            <h1 class="text-xl font-semibold">{{ selectedList.listName }}</h1>
+            <button class="text-sm bg-gray-200 px-3 py-1 rounded-md" @click="backToListView">Back to Groups</button>
+        </div>
+        <div class="space-y-2">
+            <div v-for="contact in selectedList.contacts" :key="contact.id">
+                <div class="flex justify-between items-center px-2 py-1 border-b">
+                    <p>{{ contact }}</p>
+                    <!-- Add buttons or functionality for removing contacts here -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ///////////////// -->
+    <!-- Pagination Buttons - should this be in a div?-->
+    <ol class="flex justify-center gap-1 text-xs font-medium pt-2 pb-4">
+        <li>
+            <button
+            @click="prevPage"
+            :disabled="currentListPage === 1"
+            class="inline-flex size-8 items-center justify-center rounded border border-gray-2 00 bg-gray-100 text-gray-900 rtl:rotate-180"
+            >
+                <span class="sr-only">Prev Page</span>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-3 w-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                >
+                <path
+                fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+                />
+                </svg>
+            </button>
+        </li>
+    
+        <li v-for="page in totalPages" :key="page">
+                <button
+                @click="goToPage(page)"
+                :class="{'block size-8 rounded border border-gray-2 00 bg-gray-100 text-center leading-8 text-gray-900': page !== currentListPage, 'block size-8 rounded border-my-teal bg-my-teal text-center leading-8 text-white': page === currentListPage}"
+                >
+                {{ page }}
+            </button>
+        </li>
+        <li>
+            <button
+            @click="nextPage"
+            :disabled="currentListPage === totalPages"
+            class="inline-flex size-8 items-center justify-center rounded border border-gray-200 bg-gray-100 text-gray-900 rtl:rotate-180"
+            >
+                <span class="sr-only">Next Page</span>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-3 w-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                >
+                <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a 1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+                />
+                </svg>
+            </button>
+        </li>
+    </ol>
+
+</div>
+<!-- Block - Detail View -->
+
 </template>
